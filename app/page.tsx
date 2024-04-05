472e0329c9706095e34e689a07fd02ebf2fb706d
Olivier Huvelle <olivier.huvelle@gmail.com>
2024-04-05 19:32:03 +0200

    fixed responsive issues on login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import LoginForm from '@/app/loginForm'
+import LoginForm from '@/components/authentication/loginForm'
 
 export default async function Home() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
-      <main className="mb-4 min-w-80 rounded bg-white px-8 pb-8 pt-6 shadow-md">
+      <main className="mb-4 w-full rounded bg-white px-8 pb-8 pt-6 shadow-md md:w-96">
         <h1 className="mb-4 text-center text-2xl">Connexion</h1>
         <LoginForm />
       </main>
